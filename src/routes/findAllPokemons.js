e7c41dcb5d3d0f50c1324a3198ebf26daecc97fe
heraur <kemkengaurelien@gmail.com>
2023-07-05 22:23:51 +0100

    just to save, error in getter in the model pokemon

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,16 +1,34 @@
 const { Pokemon } = require("../db/sequelize");
+const { Op } = require("sequelize");
 
 module.exports = (app) => {
   app.get("/api/pokemons", (req, res) => {
-    Pokemon.findAll()
-      .then((pokemons) => {
-        const message = "La liste des pokémons a bien été récupérée.";
-        res.json({ message, data: pokemons });
-      })
-      .catch((error) => {
-        const message =
-          "La liste des pokemons n'a pas pu etre recuperee. Reessayer dans quelques instant";
-        res.status(500).json({ meaage, data: error });
+    if (req.query.name) {
+      const name = req.query.name;
+      return Pokemon.findAndCountAll({
+        where: {
+          name: {
+            //'name' est la propriete du modele pokemon
+            [Op.like]: `%${name}%`, // 'name' est le critere de recherche
+          },
+        },
+        order: ["name"],
+        limit: 5,
+      }).then(({ count, rows }) => {
+        const message = `Il y'a ${count} pokemons correspondant au terme de recherche ${name}`;
+        res.json({ message, data: rows });
       });
+    } else {
+      Pokemon.findAll({ order: ["name"] })
+        .then((pokemons) => {
+          const message = "La liste des pokémons a bien été récupérée.";
+          res.json({ message, data: pokemons });
+        })
+        .catch((error) => {
+          const message =
+            "La liste des pokemons n'a pas pu etre recuperee. Reessayer dans quelques instant";
+          res.status(500).json({ message, data: error });
+        });
+    }
   });
 };
