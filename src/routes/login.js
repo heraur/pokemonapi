71a86648dfbd208d74050e7cb350515e21dbb023
heraur <kemkengaurelien@gmail.com>
2023-07-06 14:46:24 +0100

    fin authentification securiser

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,7 @@
 const { User } = require("../db/sequelize");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const privatekey = require("../auth/private_key");
 
 module.exports = (app) => {
   app.post("/api/login", (req, res) => {
@@ -18,8 +20,13 @@ module.exports = (app) => {
               return res.status(401).json({ message });
             }
 
+            //JWT
+            const token = jwt.sign({ userId: user.id }, privatekey, {
+              expiresIn: "24h",
+            });
+
             const message = `L'utilisateur a été connecté avec succès`;
-            return res.json({ message, data: user });
+            return res.json({ message, data: user, token });
           });
       })
       .catch((error) => {
