b85ef793b1d1eaba1f69d3da1cd245e3bce8be8a
heraur <kemkengaurelien@gmail.com>
2023-07-06 15:53:07 +0100

    prepare deployment

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const morgan = require("morgan");
 const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
 const sequelize = require("./src/db/sequelize");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app
-  .use(favicon(__dirname + "/favicon.ico"))
-  .use(morgan("dev"))
-  .use(bodyParser.json());
+app.use(favicon(__dirname + "/favicon.ico")).use(bodyParser.json());
 
 sequelize.initDb();
+
+app.get("/", (req, res) => {
+  res.json("Hello Heroku from Heraur");
+});
 
 //Ici nous placerons nos futures points de terminaison
 
